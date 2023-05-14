3d380cfff5ef0e34f9297eb45856b14e288731da
Mattias Evensson <mattias@evensson.eu>
2023-05-14 20:22:19 +0200

    Add devshell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+    });
+}
