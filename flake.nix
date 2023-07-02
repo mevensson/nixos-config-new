9f17bf025b7fdf294c5ff07647c1fc54996b0325
Mattias Evensson <mattias@evensson.eu>
2023-07-02 14:37:34 +0200

    Use flake parts

--- flake.nix
+++ flake.nix
@@ -3,50 +3,80 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, ... } @inputs: {
-    nixosConfigurations = {
-      ryzen = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/ryzen/configuration.nix
+  outputs = { self, ... } @inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
 
-          ./profiles/boot/systemd-boot.nix
-          ./profiles/graphical/gdm.nix
-          ./profiles/graphical/gnome.nix
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
 
-          ./users/matte.nix
-        ];
-      };
-      t14g1 = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/t14g1/configuration.nix
+      flake = {
+        nixosConfigurations = {
+          ryzen = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./hosts/ryzen/configuration.nix
 
-          ./profiles/boot/systemd-boot.nix
-          ./profiles/graphical/gdm.nix
-          ./profiles/graphical/gnome.nix
+              ./profiles/boot/systemd-boot.nix
+              ./profiles/graphical/gdm.nix
+              ./profiles/graphical/gnome.nix
 
-          ./users/matte.nix
-        ];
-      };
-    };
-  }
-  //
-  inputs.flake-utils.lib.eachDefaultSystem (system: {
-    devShells = {
-      default =
-        let
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-            overlays = [ inputs.devshell.overlays.default ];
+              ./users/matte.nix
+            ];
           };
-        in
-        pkgs.devshell.fromTOML ./devshell.toml;
+          t14g1 = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ./hosts/t14g1/configuration.nix
+
+              ./profiles/boot/systemd-boot.nix
+              ./profiles/graphical/gdm.nix
+              ./profiles/graphical/gnome.nix
+
+              ./users/matte.nix
+            ];
+          };
+        };
+      };
+
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem = { pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        devshells.default = {
+          commands = [
+            {
+              name = "fmt";
+              help = "Format all files.";
+              command = "nix fmt";
+            }
+            {
+              name = "check";
+              help = "Check the flake for errors.";
+              command = "nix flake check";
+            }
+          ];
+          packages = [
+            pkgs.rnix-lsp
+            pkgs.vscode
+          ];
+        };
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.nixpkgs-fmt.enable = true;
+        };
+      };
     };
-  });
 }
