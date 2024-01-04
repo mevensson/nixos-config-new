e959fefb63bb61aee10d546e1d461073c89c256b
Mattias Evensson <mattias@evensson.eu>
2024-01-04 15:49:58 +0100

    Enable and configure Gnome Appindicator extension

--- users/profiles/graphical/gnome/extensions/default.nix
+++ users/profiles/graphical/gnome/extensions/default.nix
@@ -2,24 +2,24 @@
 
 {
   imports = [
+    ./appindicator.nix
     ./blur-my-shell.nix
     ./espresso.nix
     ./extension-list.nix
     ./forge.nix
     ./gsconnect.nix
-    ./tray-icons-reloaded.nix
     ./vitals.nix
   ];
 
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
+        "appindicatorsupport@rgcjonas.gmail.com"
         "blur-my-shell@aunetx"
         "espresso@coadmunkee.github.com"
         "extension-list@tu.berry"
         "forge@jmmaranan.com"
         "gsconnect@andyholmes.github.io"
-        "trayIconsReloaded@selfmade.pl"
         "Vitals@CoreCoding.com"
       ];
     };
