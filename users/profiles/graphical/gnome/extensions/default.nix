6666097a35ff99339178959edf34d4500a29767c
Mattias Evensson <mattias@evensson.eu>
2024-01-04 02:30:55 +0100

    Enable and configure Gnome Tray Icons: Reloaded

--- users/profiles/graphical/gnome/extensions/default.nix
+++ users/profiles/graphical/gnome/extensions/default.nix
@@ -3,12 +3,14 @@
 {
   imports = [
     ./forge.nix
+    ./tray-icons-reloaded.nix
   ];
 
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
         "forge@jmmaranan.com"
+        "trayIconsReloaded@selfmade.pl"
       ];
     };
   };
