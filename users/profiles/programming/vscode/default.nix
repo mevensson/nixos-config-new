f1e8339fd6408337c6bce152bf81e1c13c211a34
Mattias Evensson <mattias@evensson.eu>
2024-08-10 09:37:29 +0200

    Enable Wayland for VSCode

--- users/profiles/programming/vscode/default.nix
+++ users/profiles/programming/vscode/default.nix
@@ -4,6 +4,11 @@
     ./extensions/redhat.vscode-yaml.nix
   ];
 
+  home.sessionVariables = {
+    # Enable Wayland for Chrome and Election apps
+    NIXOS_OZONE_WL = "1";
+  };
+
   programs.vscode = {
     enable = true;
   };
