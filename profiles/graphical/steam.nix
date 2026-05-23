df94a83bf4a26a39293e848efe0d3dc76d101b45
Mattias Evensson <mattias@evensson.eu>
2026-05-23 17:14:33 +0200

    Disable setuid for bwrap to fix steam

--- profiles/graphical/steam.nix
+++ profiles/graphical/steam.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   hardware.graphics.enable32Bit = true;
 
   programs.steam = {
     enable = true;
   };
+
+  # Disable setuid for bwrap
+  security.wrappers.bwrap.setuid = lib.mkForce false;
 }
