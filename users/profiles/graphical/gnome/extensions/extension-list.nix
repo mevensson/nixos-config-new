0e1bebd90ae84510c05cf6138348df5f96ff96de
Mattias Evensson <mattias@evensson.eu>
2026-06-03 01:52:51 +0200

    Override extension-list with latest v50.2 from upstream GitHub
    nixpkgs has extension-list at v32 (GNOME 43 only), fetched from EGO.
    Upstream has moved to a meson build system on GitHub with releases
    up to v50.2 (GNOME 50). This override builds directly from GitHub.

--- users/profiles/graphical/gnome/extensions/extension-list.nix
+++ users/profiles/graphical/gnome/extensions/extension-list.nix
@@ -1,9 +1,26 @@
 { lib, pkgs, ... }:
 
+let
+  extension-list' = pkgs.stdenv.mkDerivation {
+    pname = "gnome-shell-extension-extension-list";
+    version = "50.2";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "tuberry";
+      repo = "extension-list";
+      rev = "50.2";
+      hash = "sha256-1kZVOr8MbjN9ZYNOejH6H3LBgHF3B8W4iXPVqR6HXbQ=";
+    };
+
+    nativeBuildInputs = with pkgs; [ meson ninja pkg-config sassc ];
+    buildInputs = with pkgs; [ glib ];
+
+    mesonFlags = [ "-Dtarget=system" ];
+  };
+in
+
 {
-  home.packages = with pkgs; [
-    gnomeExtensions.extension-list
-  ];
+  home.packages = [ extension-list' ];
 
   dconf.settings = {
     "org/gnome/shell" = {
