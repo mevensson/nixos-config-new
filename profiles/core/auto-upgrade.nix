cba060914c0638a2211e0f338f18499e2ec57dc6
Mattias Evensson <mattias@evensson.eu>
2026-06-25 23:03:09 +0200

    chore: remove unused auto-upgrade module

--- profiles/core/auto-upgrade.nix
+++
@@ -1,9 +0,0 @@
-{ ... }: {
-  system.autoUpgrade = {
-    enable = true;
-    flake = "github:mevensson/nixos-config-new";
-    dates = "daily";
-    persistent = true;
-    randomizedDelaySec = "10m";
-  };
-}
